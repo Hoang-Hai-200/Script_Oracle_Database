878c1bd2a827400d2af5fa72e61e02f89344d5c5
Hoang-Hai-200 <64273754+Hoang-Hai-200@users.noreply.github.com>
2022-09-05 00:12:52 +0700

    Create create_service_pdb.sh

---
+++ create_service_pdb.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+
+sqlplus / as sysdba << EOF
+alter session set container =pdb_hr;
+
+exec dbms_service.create_service(SERVICE_NAME=>'new_hr',NETWORK_NAME=>'new_hr');
+exec dbms_service.start_service('new_hr');
