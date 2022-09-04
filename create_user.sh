fb11dd4587b41b2db56aae0e53c5998ffdd79825
Hoang-Hai-200 <64273754+Hoang-Hai-200@users.noreply.github.com>
2022-09-05 00:13:30 +0700

    Create create_user.sh

---
+++ create_user.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+sqlplus / as sysdba << EOF
+alter pluggable database pdb1 open;
+alter session set container = pdb1;
+
+create user hoang identified by foo;
+grant connect, resource, select any dictionary, unlimited tablespace to hoang;
+
+alter user hoang default tablespace users;
