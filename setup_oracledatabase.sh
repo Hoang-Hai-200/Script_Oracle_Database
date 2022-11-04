a3652eef955d0907d4fdf06c8e4b0379818759de
Hoang-Hai-200 <64273754+Hoang-Hai-200@users.noreply.github.com>
2022-11-03 17:30:01 -0700

    set up oracle database 19c

---
+++ setup_oracledatabase.sh
@@ -0,0 +1,145 @@
+:'
+cat >> /etc/sysctl.conf << EOF
+fs.file-max = 6815744
+kernel.sem = 250 32000 100 128
+kernel.shmmni = 4096
+kernel.shmall = 1073741824
+kernel.shmmax = 4398046511104
+kernel.panic_on_oops = 1
+net.core.rmem_default = 262144
+net.core.rmem_max = 4194304
+net.core.wmem_default = 262144
+net.core.wmem_max = 1048576
+net.ipv4.conf.all.rp_filter = 2
+net.ipv4.conf.default.rp_filter = 2
+fs.aio-max-nr = 1048576
+net.ipv4.ip_local_port_range = 9000 65500
+EOF
+
+
+/sbin/sysctl -p
+
+cat >> /etc/security/limits.d/oracle-database-preinstall-19c.conf << EOF
+oracle   soft   nofile    1024
+oracle   hard   nofile    65536
+oracle   soft   nproc    16384
+oracle   hard   nproc    16384
+oracle   soft   stack    10240
+oracle   hard   stack    32768
+oracle   hard   memlock    134217728
+oracle   soft   memlock    134217728
+EOF
+
+yum install -y xorg-x11-xauth
+yum install -y bc    
+yum install -y binutils
+yum install -y compat-libcap1
+yum install -y compat-libstdc++-33
+#yum install -y dtrace-modules
+#yum install -y dtrace-modules-headers
+#yum install -y dtrace-modules-provider-headers
+yum install -y dtrace-utils
+yum install -y elfutils-libelf
+yum install -y elfutils-libelf-devel
+yum install -y fontconfig-devel
+yum install -y glibc
+yum install -y glibc-devel
+yum install -y ksh
+yum install -y libaio
+yum install -y libaio-devel
+yum install -y libdtrace-ctf-devel
+yum install -y libXrender
+yum install -y libXrender-devel
+yum install -y libX11
+yum install -y libXau
+yum install -y libXi
+yum install -y libXtst
+yum install -y libgcc
+yum install -y librdmacm-devel
+yum install -y libstdc++
+yum install -y libstdc++-devel
+yum install -y libxcb
+yum install -y make
+yum install -y net-tools # Clusterware
+yum install -y nfs-utils # ACFS
+yum install -y python # ACFS
+yum install -y python-configshell # ACFS
+yum install -y python-rtslib # ACFS
+yum install -y python-six # ACFS
+yum install -y targetcli # ACFS
+yum install -y smartmontools
+yum install -y sysstat
+yum install -y unixODBC
+
+
+
+setenforce Permissive
+systemctl stop firewalld
+systemctl disable firewalld
+
+
+
+mkdir -p /u01/app/oracle/product/19.0.0/dbhome_1
+mkdir -p /u02/oradata
+chown -R oracle:oinstall /u01 /u02
+chmod -R 775 /u01 /u02
+
+#---------------------------------------------
+mkdir /home/oracle/scripts
+cat > /home/oracle/scripts/setEnv.sh <<EOF
+# Oracle Settings
+export TMP=/tmp
+export TMPDIR=\$TMP
+
+export ORACLE_HOSTNAME=ol7-19.localdomain
+export ORACLE_UNQNAME=cdb1
+export ORACLE_BASE=/u01/app/oracle
+export ORACLE_HOME=\$ORACLE_BASE/product/19.0.0/dbhome_1
+export ORA_INVENTORY=/u01/app/oraInventory
+export ORACLE_SID=cdb1
+export PDB_NAME=pdb1
+export DATA_DIR=/u02/oradata
+
+export PATH=/usr/sbin:/usr/local/bin:\$PATH
+export PATH=\$ORACLE_HOME/bin:\$PATH
+
+export LD_LIBRARY_PATH=\$ORACLE_HOME/lib:/lib:/usr/lib
+export CLASSPATH=\$ORACLE_HOME/jlib:\$ORACLE_HOME/rdbms/jlib
+EOF
+#--------------------------------------------
+
+
+#config for x11
+echo 'AddressFamily inet' >>  /etc/ssh/sshd_config 
+service sshd restart
+
+
+echo ". /home/oracle/scripts/setEnv.sh" >> /home/oracle/.bash_profile
+#-------------------------------------------
+
+
+cat > /home/oracle/scripts/start_all.sh <<EOF
+#!/bin/bash
+. /home/oracle/scripts/setEnv.sh
+
+export ORAENV_ASK=NO
+. oraenv
+export ORAENV_ASK=YES
+
+dbstart \$ORACLE_HOME
+EOF
+
+#-------------------------------------------
+cat > /home/oracle/scripts/stop_all.sh <<EOF
+#!/bin/bash
+. /home/oracle/scripts/setEnv.sh
+
+export ORAENV_ASK=NO
+. oraenv
+export ORAENV_ASK=YES
+
+dbshut \$ORACLE_HOME
+EOF
+#----------------------------------------- 
+chown -R oracle:oinstall /home/oracle/scripts
+chmod u+x /home/oracle/scripts/*.sh
